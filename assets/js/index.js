6558c2574f5cc3550c34268d4626571762c18dc4
shielh <shiel.helen@gmail.com>
2021-01-22 13:06:19 +0000

    Add buttons to breathing technique section and add toggle() on click event

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#breathing-info-1, #breathing-info-2, #breathing-info-3, #breathing-info-4").hide();
+
+    
+});
+
+$("#more-info-btn-1").click(function(){
+        $("#breathing-info-1").toggle();
+    });    
+
+$("#more-info-btn-2").click(function(){
+        $("#breathing-info-2").toggle();
+    });    
+
+$("#more-info-btn-3").click(function(){
+        $("#breathing-info-3").toggle();
+    });    
+$("#more-info-btn-4").click(function(){
+        $("#breathing-info-4").toggle();
+    });    
